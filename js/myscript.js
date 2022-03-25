3454be9754fc1f0f2bdf00d857e832f7a354441f
marlon023562 <102229130+marlon023562@users.noreply.github.com>
2022-03-25 04:06:24 -0700

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,35 @@
+﻿$(function(){
+	function load(){
+		$("#loading").html('<img src="img/loading.gif">');
+		$.ajax({
+			type: 'post',
+			url: 'php/read.php',
+			data:{				
+				
+			}
+		}).done(function(data){			
+			$("#main").html(data)
+			$("#loading").empty()
+		})
+	}
+ 
+ 
+    load()
+	$(document).on('click','#tbl1', function(){
+	alert('');
+	})
+	//$("#tbl1").fadeToggle(1000).fadeToggle(1000);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ php/read.php
@@ -0,0 +1,69 @@
+﻿<script src="/../bootstrap/js/jquery-1.11.3.js"></script>
+<div class="col-sm-6">
+   <div class="image">
+   <img src="./img/shin.jpg">
+   </div> 
+    <div class="profile_info">
+   <p>I'm Marlon E. Curitana</p>
+   <p>I'm Marlon E. Curitana</p>
+    </div>
+ </div>
+ <div class="col-sm-6">
+   <h1> Hello</h1>
+   <h2> I'm Marlon</h2>
+   <h3>-Web designer</h3>
+   <br>
+   <br>
+   <br>
+     <div class="col-sm-5">
+       <table class="table table-striped table-bordered table-hover table-condensed" id="tbl1">
+	     <thead class="bg-success">
+	       <tr>
+	       <th>fffgfg</th>
+	       <th>fffgfg</th>
+	       <th>fffgfg</th>
+	       </tr>
+		   <tr>
+	       <th>fffgfg</th>
+	       <th>fffgfg</th>
+	       <th>fffgfg</th>
+	       </tr>
+	     </thead>
+	     <tbody>
+	       <tr>
+	       <td>fgfhgfhg<td>
+	       </tr>
+	     </tbody>
+	    </table>
+      </div>
+	  
+	   <div class="col-sm-5">
+       <table class="table table-striped table-bordered table-hover table-condensed">
+	     <thead class="bg-success">
+	       <tr>
+	       <th>fffgfg</th>
+	       <th>fffgfg</th>
+	       <th>fffgfg</th>
+	       </tr>
+	     </thead>
+	     <tbody>
+	       <tr>
+	       <td>fgfhgfhg<td>
+	       </tr>
+	     </tbody>
+	    </table>
+      </div>
+ </div>
+
+
+<script>	
+	$(document).ready(function(){
+		$("#date_fin").datepicker({
+			format: 'yyyy-mm-dd',
+			startDate: '-3m',
+			autoclose: true
+		})
+	})
+</script>
+
+
